7a20a346464f34d29fc64501496435d390c6d49c
Rzbck <chauvel.charlie@gmail.com>
2020-04-08 17:17:11 +0200

    Update 03_stairway.rb

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,32 +1,46 @@
 system ("cls")
 
-step , turn , max_turn = 0 , 0 , 100
+n_step , step , turn , max_turn , tsleep , multi = 20, 0 , 0 , 100 , 0.05 , 5
 
-while step != 10
+while step != n_step
     max_turn.times {
+        system ("cls")
         turn +=1
         puts "TURN: #{turn}"
         roll = rand(1..6)    
-        puts roll
+        puts "Rolldice: #{roll}"
         if roll == 1 
-            puts "-1 Step :( Bad You are now ON STEP #{step}"
-            step -=1
+            puts "-1 Step :( Bad "
+            puts "STEP : #{step}/#{n_step} "
+            if step != 0
+                step -=1
+            end
         end
         if roll == 2  || roll == 3 || roll == 4
-            puts "nothing happen you stay ON STEP #{step}"
+            puts "nothing happen you stay "
+            puts "STEP : #{step}/#{n_step} "
         end
         if roll == 5  || roll == 6
             step +=1
-            puts "+1 Step !!! Nice you are now ON STEP #{step}"
+            puts "+1 Step !!! Nice you are now "
+            puts "STEP : #{step}/#{n_step} "
         end
-        if step == 10
-            puts "You win the game ON STEP #{step} ! CONGRATULATION !!!"
+        if step == n_step
+            puts "You win the game !! you reach STEP #{step}/#{n_step}  ! CONGRATULATION !!!"
+            puts "|" + "-" * ((step*multi)-2) + "|"
+            puts "+"*(n_step*multi) 
             break
         end
-        puts "+"*50 
+        if step != 0
+            puts "|"+"-"*((step*multi)-2)+"|"
+            puts "+"*(n_step*multi) 
+            sleep(tsleep)
+        end
         } 
     if turn == max_turn 
-        puts "You are a Fucking Looser !"
+        puts "You are a Fucking Looser ! you only reach STEP #{step}/#{n_step}  :( LOOSER !"
     end
     break
 end
+
+
